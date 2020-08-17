6b8e52457fa3907f74291b30c6cf8228c2642028
Chi-Heng Hung <jeffreyhung@Hotmail.com.tw>
2020-08-17 15:26:13 -0500

    fix csp bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -221,7 +221,7 @@ module.exports = {
         mergeStyleHashes: false, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self''unsafe-inline' www.google-analytics.com www.googletagmanager.com fonts.googleapis.com fonts.gstatic.com",
+          "script-src": "'self' 'unsafe-inline' www.google-analytics.com www.googletagmanager.com fonts.googleapis.com fonts.gstatic.com",
           "style-src": "'self' data: blob: 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com",
           "img-src": "'self' data: www.google-analytics.com",
           "font-src": "'self' fonts.gstatic.com",
