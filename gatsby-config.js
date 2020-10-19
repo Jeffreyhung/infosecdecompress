accb9c86c5b7c52fa46d95b7d90419ecb9a6808b
Chi-Heng Hung <jeffreyhung@Hotmail.com.tw>
2020-10-19 15:27:05 -0500

    Add Fonts preload

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,6 +131,7 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-preload-fonts',
     'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-plugin-netlify-cms',
